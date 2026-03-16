e0d2581740dae31079bc732a9a19720dd9094643
JustAGirl5109 <mongoosexcobra@gmail.com>
2026-03-16 10:48:28 -0700

    cganged student 2 artist

--- models/Artwork.js
+++ models/Artwork.js
@@ -8,7 +8,20 @@ const artSchema = new mongoose.Schema({
   },
   desc:String,
   display:Boolean,
+  originalName: { type: String, required: true, trim: true },
 
+  // stored filenames (no paths)
+  originalFile: { type: String, required: true, trim: true },
+  thumbFile: { type: String, required: true, trim: true },
+  largeFile: { type: String, required: true, trim: true },
+
+  // optional image info (nice for “details” panel)
+  width: { type: Number },
+  height: { type: Number },
+
+  originalBytes: { type: Number },
+  thumbBytes: { type: Number },
+  largeBytes: { type: Number },
 })
 
 export default mongoose.model("Artwork",artSchema)
